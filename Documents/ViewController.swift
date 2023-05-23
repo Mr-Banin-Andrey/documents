80ee4ea281caccc761d24b745ade8deaf941bf9b
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2023-05-23 21:44:49 +0200

    Initial Commit

---
+++ Documents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Documents
+//
+//  Created by Андрей Банин on 23.5.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
